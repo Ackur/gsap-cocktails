d71806b01b6474af681d9f6024ef93b8c80a3f98
vKremenok <vkremenok@lleida.net>
2025-07-04 14:28:38 +0200

    added local fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import { useGSAP } from "@gsap/react";
 
 import AppNavbar from "@/components/AppNavbar";
 import HeroSection from "@/components/HeroSection";
@@ -9,7 +10,7 @@ import ArtSection from "./components/ArtSection";
 import MenuSlider from "./components/MenuSlider";
 import ContactSection from "./components/ContactSection";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP);
 
 const App = () => {
   return (
